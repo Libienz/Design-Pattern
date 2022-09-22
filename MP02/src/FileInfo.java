1e56995640c7169c3840691d39b2221ec05da449
Kuen Hee Lee <Leekh10195@gmail.com>
2022-09-22 22:53:13 +0900

    MP02 in progress..

---
+++ MP02/src/FileInfo.java
@@ -0,0 +1,42 @@
+import java.util.Date;
+
+public class FileInfo {
+
+    String name;
+    String type;
+    int size;
+    Date modifiedDate;
+
+    public FileInfo(String name, String type, int size, Date modifiedDate) {
+        this.name = name;
+        this.type = type;
+        this.size = size;
+        this.modifiedDate = modifiedDate;
+    }
+
+    @Override
+    public String toString() {
+        return "FileInfo{" +
+                "name='" + name + '\'' +
+                ", type='" + type + '\'' +
+                ", size=" + size +
+                ", modifiedDate=" + modifiedDate +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public Date getModifiedDate() {
+        return modifiedDate;
+    }
+}
---
+++ MP02/src/Sorter.java
@@ -0,0 +1,53 @@
+import java.util.Arrays;
+import java.util.Date;
+
+public class Sorter {
+    Comparable compare;
+
+    public Sorter(Comparable compare) {
+        this.compare = compare;
+    }
+
+    public void setComparable(Comparable compare) {
+        this.compare = compare;
+    }
+
+    public void bubbleSort(Object[] objs) {
+        Arrays.sort(objs);
+    }
+    void bubbleSortInts(Integer[] data) {
+        for (int i = 0; i < data.length - 1; i++) {
+            for (int j = 0; j < data.length - i - 1; j++) {
+                if (data[j] > data[j + 1]) { // swap
+                    Integer temp = data[j];
+                    data[j] = data[j + 1];
+                    data[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    void bubbleSortDates(Date[] data3) {
+        for (int i = 0; i < data3.length - 1; i++) {
+            for (int j = 0; j < data3.length - i - 1; j++) {
+                if (data3[j].compareTo(data3[j + 1]) > 0) { // swap
+                    Date temp = data3[j];
+                    data3[j] = data3[j + 1];
+                    data3[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    void bubbleSortStrings(String[] data2) {
+        for (int i = 0; i < data2.length - 1; i++) {
+            for (int j = 0; j < data2.length - i - 1; j++) {
+                if (data2[j].compareTo(data2[j + 1]) > 0) { // swap
+                    String temp = data2[j];
+                    data2[j] = data2[j + 1];
+                    data2[j + 1] = temp;
+                }
+            }
+        }
+    }
+}
