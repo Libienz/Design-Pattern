fa86dc9db41a33cdae49c36dfa71d224cc7b129d
Lkh-dev <Leekh10195@gmail.com>
2022-09-27 15:04:13 +0900

    MP02 all done

--- MP02/src/Sorter.java
+++ MP02/src/Sorter.java
@@ -16,7 +16,7 @@ public class Sorter {
     obj[j] 와 obj[j+1]을 비교하는데
     j번째의 값이 j+1번째의 값보다 뒤에 있어야 하면 swap!
      */
-    public void bubbleSort(Object[] objs) { // object 클래스로 가능하드아 ..?
+    public void bubbleSort(Object[] objs) { // object 클래스로 가능하드아 ..? -> ㅇㅇ 우리에게는 다운캐스팅이 있다.
         for (int i = 0; i < objs.length - 1; i++) {
             for (int j = 0; j < objs.length - i - 1; j++) {
                 if (compare.compareTo(objs[j],objs[j+1]) > 0) { // swap
