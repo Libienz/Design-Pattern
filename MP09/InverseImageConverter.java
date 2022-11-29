133bb5d9daf0b2ed7677884d8b5bde4dfa234a12
Lkh-dev <Leekh10195@gmail.com>
2022-11-29 14:48:12 +0900

    MP09 done

--- MP09/InverseImageConverter.java
+++ MP09/InverseImageConverter.java
@@ -7,27 +7,21 @@ import java.io.IOException;
 
 //import java.imageio.*;
 
-class InverseImageConverter {
-    public static void main(String[] args) {
-        final String HOUSE = "house.png";
-    
-        try {       
-            BufferedImage image = ImageIO.read(new File(HOUSE));
+class InverseImageConverter extends ImageConverter{
 
-            for(int y = 0; y < image.getHeight(); y++) {
-               for(int x = 0; x < image.getWidth(); x++) {
-                   Color color = new Color(image.getRGB(x, y));
-                   int outputRed = 255 - color.getRed();
-                   int outputGreen = 255 - color.getGreen();
-                   int outputBlue = 255 - color.getBlue();
-                   image.setRGB(x, y, new Color(outputRed, outputGreen, outputBlue).getRGB());
-               }
+
+    @Override
+    public void convert(BufferedImage image) {
+        for(int y = 0; y < image.getHeight(); y++) {
+            for(int x = 0; x < image.getWidth(); x++) {
+                Color color = new Color(image.getRGB(x, y));
+                int outputRed = 255 - color.getRed();
+                int outputGreen = 255 - color.getGreen();
+                int outputBlue = 255 - color.getBlue();
+                image.setRGB(x, y, new Color(outputRed, outputGreen, outputBlue).getRGB());
             }
+        }
 
-            ImageIO.write(image, "png", new File("i_" + HOUSE));
-        }
-        catch (IOException e) {
-            e.printStackTrace();
-        }
+
     }
 }
